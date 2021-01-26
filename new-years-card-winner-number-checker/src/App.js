b5b3b4e532ff7764ae0d3e4c64151420d733bca4
totuus1157 <takahatam@icloud.com>
2021-01-26 15:29:08 +0900

    Appコンポーネントをclassで書き直した

--- new-years-card-winner-number-checker/src/App.js
+++ new-years-card-winner-number-checker/src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="">
-        <h1>年賀状当選番号チェッカー</h1>
-        <h2>【２０２１年版】</h2>
-      </header>
-      <main>
-        <section>
-          <form action="">
-            <label htmlFor="">番号の下２桁を入力してください：</label>
-            <input id="input_number" type="text" autoFocus maxLength="2" minLength="2" pattern="[0-9]{2}" size="2" />
-            <input type="submit" value="判定！" />
-          </form>
-        </section>
-        <section>
-          <p></p>
-        </section>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  
+  render() {
+    return (
+      <div className="">
+        <header className="">
+          <h1>年賀状当選番号チェッカー</h1>
+          <h2>【２０２１年版】</h2>
+        </header>
+        <main>
+          <section>
+            <form action="">
+              <label htmlFor="">番号の下２桁を入力してください：</label>
+              <input id="input_number" type="text" autoFocus maxLength="2" minLength="2" pattern="[0-9]{2}" size="2" />
+              <input type="submit" value="判定！" />
+            </form>
+          </section>
+          <section>
+            <p></p>
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
