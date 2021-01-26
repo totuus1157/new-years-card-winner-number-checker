e5c81ab79f6f42e8b4da68bdda307f7565aa4910
totuus1157 <takahatam@icloud.com>
2021-01-26 11:42:11 +0900

    基本となるフォームを作成

--- new-years-card-winner-number-checker/src/App.js
+++ new-years-card-winner-number-checker/src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="">
         <h1>年賀状当選番号チェッカー</h1>
         <h2>【２０２１年版】</h2>
       </header>
       <main>
-        
+        <section>
+          <form action="">
+            <label htmlFor="">番号の下２桁を入力してください：</label>
+            <input id="input_number" type="text" autoFocus maxLength="2" minLength="2" pattern="[0-9]{2}" size="2" />
+            <input type="submit" value="判定！" />
+          </form>
+        </section>
+        <section>
+          <p></p>
+        </section>
       </main>
     </div>
   );
